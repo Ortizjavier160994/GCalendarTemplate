6fef6e8268104cfec6dbfccb647ddf26bbf44a55
PeronsHands <ortizjavier160994@gmail.com>
2020-05-20 14:49:31 -0300

    indx

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "hello"
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
