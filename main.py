35e335707eaf492e74fe4f5feb0beae2fbebf3f0
PeronsHands <ortizjavier160994@gmail.com>
2020-05-20 19:14:20 -0300

    dates added

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
+from flask_datepicker import datepicker
 from urllib.parse import quote
 
 
 app = Flask(__name__)
+Bootstrap(app)
+datepicker(app)
 
 def getEncoded(title,detail):
 	return "https://calendar.google.com/calendar/render?action=TEMPLATE&text=" +\
 		quote(title, safe='') + "&details=" + quote(detail, safe='')
 
 
+
+
 @app.route("/",methods=('GET','POST'))
 def home():
 	if (request.method == "POST"):
-		print(request.form.get("start-date"))
+		print("submitted")
+		print(request.form.get("start"))
+		print(request.form.get("end"))
 		return getEncoded(request.form.get("title"),request.form.get("detail"))
 	return render_template("index.html")
 
