735ccf2dcad9c4a1f4e1b307c0ca1103bf3f31d3
PeronsHands <ortizjavier160994@gmail.com>
2020-05-20 17:16:37 -0300

    adding bootstrap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
 from urllib.parse import quote
+
 
 app = Flask(__name__)
 
@@ -11,6 +13,7 @@ def getEncoded(title,detail):
 @app.route("/",methods=('GET','POST'))
 def home():
 	if (request.method == "POST"):
+		print(request.form.get("start-date"))
 		return getEncoded(request.form.get("title"),request.form.get("detail"))
 	return render_template("index.html")
 
