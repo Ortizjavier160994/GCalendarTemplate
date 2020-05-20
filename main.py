21f09c8cf4d563a4082ded6864c8a46bc6723f4a
PeronsHands <ortizjavier160994@gmail.com>
2020-05-20 15:18:52 -0300

    text editor js

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["POST","GET"])
 def home():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
