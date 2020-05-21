75d82996583e1ad20b76b0362fbcbfaa944be1d3
PeronsHands <ortizjavier160994@gmail.com>
2020-05-20 21:28:34 -0300

    lang added

---
+++ templates/date.pt-BR.js
@@ -0,0 +1,14 @@
+/**
+ * Brazilian translation for bootstrap-datetimepicker
+ * Cauan Cabral <cauan@radig.com.br>
+ */
+;(function($){
+	$.fn.datetimepicker.dates['es-AR'] = {
+		days: ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"],
+		daysShort: ["Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sáb", "Dom"],
+		daysMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa", "Do"],
+		months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+		monthsShort: ["Jan", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Seo", "Oct", "Nov", "Dic"],
+		today: "Hoy"
+	};
+}(jQuery));
