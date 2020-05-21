2cf305dca89a07a5b6cf3c658638630dc70c67b0
PeronsHands <ortizjavier160994@gmail.com>
2020-05-21 10:53:41 -0300

    first send directory

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, send_from_directory
 from flask_bootstrap import Bootstrap
 from flask_datepicker import datepicker
 from urllib.parse import quote
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+
+app.config["css"] = "/home/mariano/Documents/Heroku/GCalendarTemplate/templates/"
+app.config["js"] = "/home/mariano/Documents/Heroku/GCalendarTemplate/templates/
+
 Bootstrap(app)
 datepicker(app)
+
 
 def getEncoded(title,detail):
 	return "https://calendar.google.com/calendar/render?action=TEMPLATE&text=" +\
@@ -17,6 +22,8 @@ def getEncoded(title,detail):
 
 @app.route("/",methods=('GET','POST'))
 def home():
+#	app.send_static_file('./templates/bootstrap-combined.min.css')
+#	send_css('/templates/bootstrap-combined.min.css')
 	if (request.method == "POST"):
 		print("submitted")
 		print(request.form.get("start"))
@@ -24,6 +31,14 @@ def home():
 		return redirect(getEncoded(request.form.get("title"),request.form.get("detail")))
 	return render_template("index.html")
 
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory(app.config["css"], filename = path, as_attachment=False)
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return ssend_from_directory(app.config["js"], filename = path, as_attachment=False)
 
 if __name__ == "__main__":
     app.run(debug=True)
