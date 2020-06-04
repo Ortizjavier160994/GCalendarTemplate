eabbb71686c67bd16eeb23eb6b00c4c9cba9db53
PeronsHands <ortizjavier160994@gmail.com>
2020-06-04 09:01:01 -0300

    refactor request

--- main.py
+++ main.py
@@ -67,13 +67,10 @@ def create_bitly():
 	global api_key
 	print(url)
 	if (request.method == "POST"):
-		print("POST bitly")
-		print("API",api_key)
-		print(request.form.get("new_url"))
-		print(request.form.get("new_url_title"))
-		payload = {'domain': quote("bit.ly/"+request.form.get("new_url"), safe =""), 'title': quote(request.form.get("new_url_title"), safe= ''), 'long_url': url}
-		headers = {'Authorization' : "Bearer " + api_key, "Content-Type": 'application/json'}
+		payload = {'domain': "bit.ly/"+request.form.get("new_url"), 'title': request.form.get("new_url_title"), 'long_url': url}
+		headers = {'Authorization' : "Bearer " + api_key}
 		r = requests.post("https://api-ssl.bitly.com/bitlinks", json=payload, headers = headers)
+		print(r.url)
 		print(r.content)
 		if(r.status_code == 200):
 			flash("Bien ahí perraca", "Success")
